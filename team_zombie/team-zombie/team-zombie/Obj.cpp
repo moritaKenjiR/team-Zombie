9725d31ce3cde9bbd032f3e37360f00fb68f9c35
moritaKenjiR <dotfuture.bm2dx@gmail.com>
2019-04-23 15:43:32 +0900

    test

---
+++ team_zombie/team-zombie/team-zombie/Obj.cpp
@@ -0,0 +1,112 @@
+#include "Obj.h"
+#include "GameTask.h"
+#include "ImageMng.h"
+#include "DxLib.h"
+
+Obj::Obj()
+{
+}
+
+//weak_ptrを使ってObjとCamreaを連結させる
+//Obj::Obj(const char(&_keyData)[256], const char(&_keyDataOld)[256], VECTOR2 chipOffset) 
+//	:keyData(_keyData), keyDataOld(_keyDataOld)
+//{
+//	pos		= { 32,32 };
+//	divSize = { 0,0 };
+//	divCnt  = { 0,0 };
+//	turn = false;
+//}
+
+Obj::~Obj()
+{
+	//delete imageID;
+	//delete ﾒﾓﾘのﾌﾞﾛｯｸ開放
+}
+
+void Obj::init(std::string filename, VECTOR2 divSize, VECTOR2 divCnt, VECTOR2 chipOffset, int speed, int animSpeed, int stateAnimDiv)
+{
+	//ImageMng::GetInstance()->GetID(filename, divSize, divCnt, chipOffset);
+	imageName			= filename;
+	this->divSize		= divSize;
+	this->divCnt		= divCnt;
+	this->chipOffset	= chipOffset;
+	this->speed			= speed;
+	//this->animCnt		= animCnt;
+	this->animSpeed		= animSpeed;
+	this->stateAnimDiv	= stateAnimDiv;
+	animCnt				= 0;
+
+	//int flag;
+	//imageID = new(divCnt.x * divCnt.y);
+	//imageID.resize(divCnt.x * divCnt.y);	
+	//resizeで確保して中に入れることができる
+	//resize前にデータが残っていた場合、それらが破棄される
+	//LoadDivGraph(filename.c_str(), divCnt.x * divCnt.y, divCnt.x, divCnt.y, divSize.x, divSize.y, &imageID[0]);
+}
+
+const VECTOR2 & Obj::GetPos(void)
+{
+	return this->pos;
+}
+
+void Obj::SetPos(const VECTOR2 & pos)
+{
+	this->pos = pos;
+}
+
+bool Obj::Update(void)
+{
+	return false;
+}
+
+void Obj::Draw(void)
+{
+	if (imageName.length() == 0)
+	{
+		return;
+	}
+
+	//VECTOR2 drawOffset = VECTOR2(GameTask::GetInstance().GetDrawOffset()) + VECTOR2(lpGameTask.DrawOffset());
+	//DrawGraph(pos.x + drawOffset.x, pos.y + drawOffset.y, IMAGE_ID(imageName)[chipOffset.x + divCnt.x * chipOffset.y], true);
+}
+
+void Obj::Draw(int id)
+{
+	if (imageName.length() == 0)
+	{
+		return;
+	}
+	/*VECTOR2 DrawOffset = VECTOR2(lpGameTask.GetDrawOffset().x, lpGameTask.GetDrawOffset().y) + VECTOR2(lpGameTask.DrawOffset());
+	DrawGraph(pos.x + DrawOffset.x, pos.y + DrawOffset.y, IMAGE_ID(imageName)[id], true);*/
+}
+
+//
+bool Obj::SetAnim(std::string _AnimName)
+{
+	if (animMap.find(_AnimName) == animMap.end())
+	{
+		return false;
+	}
+	if (Obj::animName != _AnimName)
+	{
+		animCnt = 0;
+		Obj::animName = _AnimName;
+	}
+	return true;
+}
+
+bool Obj::AddAnim(std::string AnimName, int id_x, int id_y, int flame, int waitCnt)
+{
+	animMap[AnimName][ANIM_START_ID] = id_x + id_y * divCnt.x;
+	animMap[AnimName][ANIM_FLAME] = flame;
+	animMap[AnimName][ANIM_WAIT] = waitCnt;
+
+	return false;
+}
+//
+
+//C++ では変数からのメンバ関数呼び出しはドット演算子、 ポインタからのメンバ関数呼び出しはアロー演算子
+
+/*EditCarsolの継承ｸﾗｽを作る
+一回押したら３６ﾄﾞｯﾄ動く、長押しは反応しない
+*/
---
+++ team_zombie/team-zombie/team-zombie/Obj.h
@@ -0,0 +1,57 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+#include "VECTOR2.h"
+
+//±ÆÒ°¼®İ@ID,@ºÏ”,@±ÆÒ°¼®İ‚ÌŠÔŠu
+enum ANIM_TBL
+{
+	ANIM_START_ID,
+	ANIM_FLAME,
+	ANIM_WAIT,
+	ANIM_MAX
+};
+
+class Obj
+{
+public:
+	Obj();
+	//Obj(const char(&_keyData)[256], const char(&_keyDataOld)[256], VECTOR2 chipOffset);
+	~Obj();
+	void init(std::string filename, VECTOR2 divSize, VECTOR2 divCnt, VECTOR2 chipOffset, int speed, int animSpeed, int stateAnimDiv);
+
+	const VECTOR2& GetPos(void);
+	void SetPos(const VECTOR2& pos);
+
+	virtual bool Update(void);		//XVˆ—
+	virtual void Draw(void);		//•`‰æŠÖ”
+	void Draw(int id);
+	bool SetAnim(std::string _AnimName);
+private:
+	//–¼‘O‹óŠÔ::ÃİÌßÚ°Ä–¼<Œ^> •Ï”–¼;
+	// std::vector<Œ^> •Ï”–¼;
+	
+	//
+	std::map<std::string, int[ANIM_MAX]> animMap;
+	std::string animName;					// ±ÆÒ°¼®İ‚Ì–¼‘O
+	//
+protected:
+	std::string imageName;
+	//const char(&keyData)[256];
+	//const char(&keyDataOld)[256];
+	VECTOR2 pos;			//•`‰æ‚ÌˆÊ’u
+	int speed;
+	VECTOR2 divSize;		//•`‰æÁ¯Ìß‚Ì»²½Ş
+	VECTOR2 divCnt;			//•`‰æÁ¯Ìß‚Ì”(c‰¡j
+	VECTOR2 chipOffset;		//•`‰æÁ¯Ìß‚Ì²Ò°¼Ş
+	VECTOR2 drawOffset;
+	int animCnt;
+	int animSpeed;
+	int stateAnimDiv;
+
+	bool AddAnim(std::string AnimName, int id_x, int id_y, int flame, int waitCnt);
+	bool turn;
+};
+
