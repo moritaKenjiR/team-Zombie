5a9f902b6f9e592f3bc2bd75cae6701bbd8e3b58
xAcerolax <isopann1221@gmail.com>
2019-04-26 15:42:19 +0900

    playr,vector2

---
+++ arimura/VECTOR2.h
@@ -0,0 +1,68 @@
+#pragma once
+#include <cmath>
+class VECTOR2
+{
+public:
+	VECTOR2();
+	VECTOR2(int x, int y);
+	~VECTOR2();
+	int x;
+	int y;
+	float fx;/////////////////
+	float fy;/////////////////////
+
+	//³‹K‰»
+	float length() const;
+	void normalize();
+
+	//‘ã“ü‰‰Zq
+	VECTOR2& operator=(const VECTOR2& vec);
+
+	//”äŠr‰‰Zq	^‹U‚ğ–â‚¤‚Æ‚«‚Íbool
+	bool operator==(const VECTOR2& vec) const;
+	bool operator!=(const VECTOR2& vec) const;
+	bool operator<=(const VECTOR2& vec) const;
+	bool operator<(const VECTOR2& vec) const;
+
+	//“Y‚¦š‰‰Zq
+	int& operator[](int i);
+
+	//’P€‰‰Zq
+	VECTOR2& operator += (const VECTOR2& vec);
+	VECTOR2& operator -= (const VECTOR2& vec);
+	VECTOR2& operator *= (int k);
+	VECTOR2& operator /= (int k);
+	VECTOR2& operator /= (float k);			///////////////////////////////////
+	VECTOR2 operator +() const;
+	VECTOR2 operator -() const;
+};
+
+//ƒxƒNƒgƒ‹‚Ì‰‰Z
+//VECTOR2 + VECTOR2				«‘å‘Ì2ŒÂ‚Ü‚Å@‚PŒÂ‚Ìê‡‚à‚ ‚é
+//•Ô‚è’l‚ÌŒ^ operator+‰‰Zq(ˆø”‚P,ˆø”2....);
+//VECTOR2 + VECTOR2
+VECTOR2 operator+(const VECTOR2& u, const VECTOR2& v);
+
+//VECTOR2 - VECTOR2
+VECTOR2 operator-(const VECTOR2& u, const VECTOR2& v);
+//int‚ÆVECTOR2‚ÌŒvZ
+//int * VECTOR2	
+VECTOR2 operator*(int k, const VECTOR2& v);
+
+VECTOR2 operator+(int k, const VECTOR2& v);
+
+VECTOR2 operator-(int k, const VECTOR2& v);
+
+
+//Vector3D * int
+VECTOR2 operator*(const VECTOR2& v, int k);//©‹t‚ğ—pˆÓ‚µ‚Ä‚½‚Ù‚¤‚ª•Ö—˜
+										   //Vector3D / int
+VECTOR2 operator/(const VECTOR2& v, int k);//VECTOR2‚ÅŠ„‚é‚±‚Æ‚Í‚Å‚«‚È‚¢‚©‚ç1ŒÂ‚¾‚¯
+
+VECTOR2 operator%(const VECTOR2& v, int k);
+
+VECTOR2 operator+(const VECTOR2& v, int k);
+
+VECTOR2 operator-(const VECTOR2& v, int k);
+
+
