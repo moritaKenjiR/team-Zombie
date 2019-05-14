de5e93c5f0f90774136eb58789efbbffb00922c0
xAcerolax <isopann1221@gmail.com>
2019-05-14 15:28:19 +0900

    追加

--- arimura/copy/team-zombie/Player.h
+++ arimura/copy/team-zombie/Player.h
@@ -8,6 +8,16 @@ struct WIRE {
 	VECTOR2 pos;
 };
 
+enum class STATE_P
+{
+	RUN,
+	JUMP,
+	FDOWN,
+	SET_WIRE,
+	WIRE,
+	MAX
+};
+
 class Camera;
 
 class Player :
@@ -15,7 +25,6 @@ class Player :
 {
 public:
 	Player();
-	//Player(const char(&_keyData)[256], const char(&_keyDataOld)[256], VECTOR2 chipOffset);
 	~Player();
 
 	bool Update(void);
@@ -23,23 +32,38 @@ public:
 	void Draw(void);
 	bool SetWire(void);
 	bool Wire(void);
+
+	void ChangeState_P(STATE_P state_p)
+	{
+		this->state_p = state_p;
+	};
+	STATE_P GetState(void) { return state_p; }
+
 private:
 	WIRE wire;
+	STATE_P state_p;
 	bool initAnim(void);
 	int animAdd;
 	char keyData[256];
 	char keyDataOld[256];
-	bool Jumpflag;		//ジャンプ中フラグ
-	bool Readyflag;		//ワイヤー準備フラグ
-	bool Wireflag;		//ワイヤー中フラグ
-	bool DownFlag;		//ワイヤー中の落下フラグ
-	//bool Wireflag;
 	
 	VECTOR2 mPos;
+	VECTOR2 vec;
 	int wireCnt;	//わいやーくーるたいむ
 	int wireTime;	//ワイヤーのクールタイム
 	float jump;
+	float grav;
 	float wireSpeed;
+
+
+
+	int (Player::*StateTbl[(int)STATE_P::MAX])(void);
+	//ﾌﾟﾚｲﾔｰのムーブ状態の保持
+	int StateRun(void);
+	int StateJump(void);
+	int StateFdown(void);
+	int StateSetWire(void);
+	int StateWire(void);
 
 	std::shared_ptr<MouseCtl> mc;
 };
