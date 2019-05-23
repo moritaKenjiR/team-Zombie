5b41d0bcaa7c0d0c4ba2872fada1d500da7fd731
タグチダイキ <lostover2112@gmail.com>
2019-05-23 17:22:31 +0900

    aaaaa

---
+++ taguchi/arimura/最新/team-Zombie/Enemy.cpp
@@ -0,0 +1,114 @@
+#include "Dxlib.h"
+#include "Enemy.h"
+#include "MapCtl.h"
+#include "EnemyAI.h"
+
+
+
+Enemy::Enemy()
+{
+	pos = { 0,0 };
+	animAdd = 0;
+	aclCnt = 0;
+	count = 0;
+	jump = -12.0f;
+	state = STATE::IDLE;
+	EmodeTbl[(int)STATE::IDLE] = &Enemy::stateIdle;
+	EmodeTbl[(int)STATE::RUN] = &Enemy::stateRun;
+	EmodeTbl[(int)STATE::JUMP] = &Enemy::stateJump;
+	EmodeTbl[(int)STATE::FDOWN] = &Enemy::stateFDown;
+	EmodeTbl[(int)STATE::DAMAGE] = &Enemy::stateDamage;
+	EmodeTbl[(int)STATE::ATTACK] = &Enemy::stateAttack;
+}
+
+
+Enemy::~Enemy()
+{
+}
+
+bool Enemy::Update(void)
+{
+	memcpy(keyDataOld, keyData, sizeof(keyDataOld));
+	GetHitKeyStateAll(keyData);
+	lpEnemyAI.CreateMove((*this));
+	//SetMove();
+	animAdd = 0;
+	(this->*EmodeTbl[(int)state])();
+	animAdd = 1;
+	animCnt += animAdd;
+	return true;
+}
+
+void Enemy::Draw(void)
+{
+	//SetDrawBlendMode(DX_BLENDMODE_ALPHA, 0);
+	Obj::Draw();
+	//SetDrawBlendMode(DX_BLENDMODE_NOBLEND,0);
+	DrawBox(0, 40, 0 + aclCnt, 80, 0x000000, true);
+	//DrawString(0, 0, "Enter 移動　Space ジャンプ　Up 停止", 0xffffff);
+	
+}
+
+bool Enemy::initAnim(void)
+{
+	AddAnim("歩く", 1, 0, 6, 7);
+	AddAnim("ジャンプ", 0, 2, 8, 12);
+	AddAnim("停止", 0, 0, 1, 1);
+	AddAnim("攻撃", 0, 2, 4, 12);
+	return true;
+}
+
+int Enemy::stateIdle(void)
+{
+	SetAnim("停止");
+	return 0;
+}
+
+int Enemy::stateRun(void)
+{
+	pos.x += speed;
+	jump = -12.0f;
+	SetAnim("歩く");
+
+	return 0;
+}
+
+int Enemy::stateJump(void)
+{
+	pos.y += jump;
+	pos.x += speed;
+	jump += 0.3f;
+	animAdd = 0;
+	SetAnim("ジャンプ");
+	animAdd = 1;
+	
+	return 0;
+}
+
+int Enemy::stateFDown(void)
+{
+	pos.y += 3.0f;
+	return 0;
+}
+
+int Enemy::stateDamage(void)
+{
+	//enemyはダメージを食らうと60フレーム間行動を停止し、そのフレーム間にaclCntを減らされる　
+	//一定数aclCntを減らされるとスピードが減少する
+	aclCnt -= 0.3f;
+	if (aclCnt <= 0.0f)
+	{
+		speed -= 2;
+		aclCnt = ACCELMAX;
+	}
+	SetAnim("停止");
+	count++;
+	return 0;
+}
+
+int Enemy::stateAttack(void)
+{
+
+	SetAnim("攻撃");
+	return 0;
+}
---
+++ taguchi/arimura/最新/team-Zombie/EnemyAI.h
@@ -0,0 +1,71 @@
+#pragma once
+#include <memory>
+#include <vector>
+#include <queue>
+#include <tuple>
+#include "VECTOR2.h"
+
+
+#define lpEnemyAI EnemyAI::GetInstance()
+constexpr int INF = 10000000;
+
+class Obj;
+class Enemy;
+enum class STATE;
+
+struct Edge {
+	int to;
+	float cost = 1;
+	Edge() {};
+	Edge(int to, float cost) :to(to), cost(cost) {};
+};
+
+struct Node {
+	VECTOR2 pos;
+	float cost;
+	bool flag = false;
+	Node() {};
+	Node(VECTOR2 pos, float cost) :pos(pos),cost(cost) {};
+};
+
+class EnemyAI
+{
+public:
+	static EnemyAI &GetInstance(void)
+	{
+		static EnemyAI getInstance;
+		return getInstance;
+	}
+
+	//ダイクストラ経路探索
+	bool Dijkstra(const VECTOR2&,const VECTOR2&);
+	bool NormalizeList(std::vector<std::vector<Node>>&, const int&);
+
+	STATE CheckDist(VECTOR2&, Enemy&);
+	bool SetTarget(std::weak_ptr<Obj>);
+	//状況からエネミーの動き方を決定する関数
+	void CreateMove(Enemy&);
+	//shortestPathMapの初期化用関数
+	void CreateShortestMap(void);
+	bool CreateTopograMap(void);
+	void SetMapListPtr(const VECTOR2 &);
+	void Draw(void);
+
+private:
+	EnemyAI();
+	EnemyAI(const EnemyAI&) {};
+	EnemyAI &operator=(const EnemyAI&) {};
+	~EnemyAI();
+
+	//最短経路探索用map
+	std::vector<std::vector<Edge>> shortestPathMap;
+	std::vector<Node> dist;
+	std::vector<std::pair<float, bool>> topograMap;
+	Node node;
+	std::weak_ptr<Obj> player;
+	VECTOR2 mapSize;
+	int count = 0;
+	int searchChipSize;
+
+};
+
