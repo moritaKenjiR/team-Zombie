3aa9269e50a1630f5b41505171a8e01c378e56a7
MOMO <senri990102@icloud.com>
2019-05-31 10:51:47 +0900

    更新

---
+++ senri/合体したやつ/team-Zombie/BaseScene.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <memory>
+#include "DxLib.h"
+#include "MouseCtl.h"
+
+struct BaseScene;
+using BASE = std::unique_ptr<BaseScene>;
+
+
+struct BaseScene
+{
+public:
+	~BaseScene();
+
+	virtual int Init() = 0;
+	virtual BASE Update(BASE &_this, const std::shared_ptr<MouseCtl> _mouseCtl) = 0;
+	virtual bool LoadProcess() = 0;
+	virtual bool EndProcess() = 0;
+	
+	std::shared_ptr<MouseCtl>mosueCtl;
+	MOUSE_INT mouseBtn;
+};
+
